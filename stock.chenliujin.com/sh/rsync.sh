93afa1c21cf529eb0dc595fd5a1352c89a81ce83
donald.chen <liujin.chen@qq.com>
2018-05-25 23:58:59 +0800

    rm

--- stock.chenliujin.com/sh/rsync.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-rsync -avz ../Application 	/data/httpd/htdocs/www.chenliujin.com/
-rsync -avz ../Common 		/data/httpd/htdocs/www.chenliujin.com/
-rsync -avz ../model 		/data/httpd/htdocs/www.chenliujin.com/
-rsync -avz ../Public 		/data/httpd/htdocs/www.chenliujin.com/
